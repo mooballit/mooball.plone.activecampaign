a2eb32ea95daf96d6ebe015674790ad677b31cff
Roman Joost <roman@mooball.com>
2012-03-02 15:11:29 +1000

    Creating interface definition.

---
+++ docs/index.rst
@@ -0,0 +1,30 @@
+.. mooball.plone.activecampaign documentation master file, created by
+   sphinx-quickstart on Thu Mar  1 13:32:55 2012.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+==============================
+ mooball.plone.activecampaign
+==============================
+
+This package integrates Plone with ActiveCampaign.
+
+API
+===
+
+.. autointerface:: mooball.plone.activecampaign.interfaces.IActiveCampaignTool
+    :members:
+
+.. autointerface:: mooball.plone.activecampaign.interfaces.IActiveCampaignSubscriber
+    :members:
+
+.. automodule:: mooball.plone.activecampaign.tool
+    :members:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- mooball/plone/activecampaign/interfaces.py
+++ mooball/plone/activecampaign/interfaces.py
@@ -1,4 +1,10 @@
 import zope.interface
+import zope.schema
+
+
+UNCONFIRMED = 0
+ACTIVE = 1
+UNSUBSCRIBED = 2
 
 
 class IActiveCampaignTool(zope.interface.Interface):
@@ -7,8 +13,39 @@ class IActiveCampaignTool(zope.interface.Interface):
     (eg.for mailing list subscriptions).
     """
 
-    def synchronise_user_data(data):
-        """synchronise given user data."""
+    def add_subscriber(subscriber, lists):
+        """
+        Adds given instance which provides
+        :class:`IActiveCampaignSubscriber` to the given lists.
+
+        :param subscriber: A :class:`IActiveCampaignSubscriber` instance
+        :param lists: The `lists` should be a list of tuples (listid,
+                      status). The `status` should be either
+                      :const:`UNCONFIRMED`, :const:`ACTIVE`,
+                      :const:`UNSUBSCRIBED`.
+
+        :rtype: None
+        :raises: raises ``AssertionError`` if given subscriber does not
+                 provide :class:`IActiveCampaignSubscriber`
+
+        """
+
+    def get_list_ids():
+        """
+        Returns all ids of available mailing lists.
+
+        :rtype: list of ids
+        """
+
+    def get_api_key():
+        """
+        Returns the API key or None if no api key is set.
+        """
+
+    def get_api_url(self):
+        """
+        Returns the API URL or None if no URL is set.
+        """
 
 
 class IActiveCampaignUser(zope.interface.Interface):
@@ -16,3 +53,36 @@ class IActiveCampaignUser(zope.interface.Interface):
     Marker interface for users who can be subscribed/unsubscribed to
     active campaign.
     """
+
+
+class IActiveCampaignSubscriber(zope.interface.Interface):
+    """
+    A subscriber which can be added/removed from mailing lists.
+    """
+
+    def get_listids():
+        """
+        Returns list ids for given subscriber.
+
+        :rtype: Returns a list of tuples for each mailing list this
+                subscriber is subscribed to, e.g. (listid, status)
+        """
+
+    email = zope.schema.TextLine(
+        title=u'Email',
+    )
+
+    first_name = zope.schema.TextLine(
+        title=u'First Name'
+    )
+
+    last_name = zope.schema.TextLine(
+        title=u'Last Name'
+    )
+
+    listids = zope.schema.List(
+        title=u'Mailing lists',
+        description=(u'A list of tuples of mailing list ids the'
+                     ' subscriber should be added to with status, e.g.'
+                     ' (listid, status)')
+    )
