704c123b99c4d08ce3e283afa398d53903b6019b
Roman Joost <roman@mooball.com>
2012-03-05 10:20:56 +1000

    Support retrieving via a getUtility call

--- mooball/plone/activecampaign/tests/test_tool.py
+++ mooball/plone/activecampaign/tests/test_tool.py
@@ -1,11 +1,12 @@
 from mooball.plone.activecampaign.testing import\
         ACTIVECAMPAIGN_FUNCTIONAL_TESTING
+from Products.CMFCore.utils import getToolByName
+from mooball.plone.activecampaign.interfaces import IActiveCampaignSubscriber
+from mooball.plone.activecampaign.interfaces import IActiveCampaignTool
+from mooball.plone.activecampaign.tool import ActiveCampaignSubscriber
+from mooball.plone.activecampaign.tool import ActiveCampaignTool
 import unittest
 import zope.interface
-from mooball.plone.activecampaign.tool import ActiveCampaignTool
-from mooball.plone.activecampaign.tool import ActiveCampaignSubscriber
-from mooball.plone.activecampaign.interfaces import IActiveCampaignTool
-from mooball.plone.activecampaign.interfaces import IActiveCampaignSubscriber
 
 
 class TestToolInterfaces(unittest.TestCase):
@@ -17,3 +18,18 @@ class TestToolInterfaces(unittest.TestCase):
         self.assertTrue(
             zope.interface.verify.verifyClass(
                 IActiveCampaignSubscriber, ActiveCampaignSubscriber))
+
+
+class TestTool(unittest.TestCase):
+
+    layer = ACTIVECAMPAIGN_FUNCTIONAL_TESTING
+
+    def test_retrieval(self):
+        tool = getToolByName(self.layer['portal'],
+                             'portal_activecampaign')
+        self.assertTrue(
+            IActiveCampaignTool.providedBy(tool))
+
+        tool = zope.component.getUtility(IActiveCampaignTool)
+        self.assertTrue(
+            IActiveCampaignTool.providedBy(tool))
