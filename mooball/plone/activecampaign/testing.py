bd68e253c2ada6dcf2ede9c5ef7c30a632f13c65
Roman Joost <roman@mooball.com>
2012-03-07 15:48:00 +1000

    Add and remove lists on setup/tearDown.
    This will unfortunately not work, as the list ids are incremented on
    every creation, therefore we'll run out of lists in the future :(

--- mooball/plone/activecampaign/testing.py
+++ mooball/plone/activecampaign/testing.py
@@ -1,3 +1,4 @@
+from Products.CMFCore.utils import getToolByName
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import applyProfile
@@ -5,6 +6,10 @@ from plone.app.testing.layers import FunctionalTesting
 from plone.app.testing.layers import IntegrationTesting
 from zope.configuration import xmlconfig
 import mooball.plone.activecampaign
+import os
+
+
+TESTLISTID = 'mooball-ac-testlist'
 
 
 class ActiveCampaignBase(PloneSandboxLayer):
@@ -18,6 +23,22 @@ class ActiveCampaignBase(PloneSandboxLayer):
     def setUpPloneSite(self, portal):
         # install into the Plone site
         applyProfile(portal, 'mooball.plone.activecampaign:default')
+
+        # configure test environment
+        ac_tool = getToolByName(portal, 'portal_activecampaign')
+        keys = ['api_url', 'api_user', 'api_password']
+        # this obviously dies if the developer hasn't set the
+        # correct environment variables, but we can't run tests with
+        # half of the information we really need.
+        options = [(k, os.environ[k]) for k in keys]
+        ac_tool.manage_changeProperties(**dict(options))
+
+        # create a test list
+        self.listid = ac_tool.add_list(TESTLISTID, 'Mooball Plone AC TestList')
+
+    def tearDownPloneSite(self, portal):
+        ac_tool = getToolByName(portal, 'portal_activecampaign')
+        ac_tool.delete_lists([self.listid])
 
 
 ACTIVECAMPAIGN_FIXTURE = ActiveCampaignBase()
