cfa2c8385c2842938d1300528c71c92ecb2060b5
Roman Joost <roman@mooball.com>
2012-03-08 09:22:12 +1000

    Not needed.

--- mooball/plone/activecampaign/interfaces.py
+++ mooball/plone/activecampaign/interfaces.py
@@ -7,36 +7,6 @@ ACTIVE = 1
 UNSUBSCRIBED = 2
 
 
-ERROR_CODES = {
-    0: "Unknown response code. Please resubmit the subscription form.",
-    1: "This list is currently not accepting subscribers. This list has met its top number of allowed subscribers.",
-    2: "Your subscription request for this list could not be processed as you are missing required fields.",
-    3: "This e-mail address is already subscribed to this mailing list.",
-    4: "This e-mail address has been processed in the past to be subscribed, however your subscription was never confirmed.",
-    5: "This e-mail address cannot be added to list.",
-    6: "This e-mail address has been processed. Please check your email to confirm your subscription.",
-    7: "This e-mail address has subscribed to the list.",
-    8: "E-mail address is invalid.",
-    9: "Subscription could not be processed since you did not select a list. Please select a list and try again.",
-    10: "This e-mail address has been processed. Please check your email to confirm your unsubscription.",
-    11: "This e-mail address has been unsubscribed from the list.",
-    12: "This e-mail address was not subscribed to the list.",
-    13: "Thank you for confirming your subscription.",
-    14: "Thank you for confirming your unsubscription.",
-    15: "Your changes have been saved.",
-    16: "Your subscription request for this list could not be processed as you must type your name.",
-    17: "This e-mail address is on the global exclusion list.",
-    18: "Please type the correct text that appears in the image.",
-    19: "Subscriber ID is invalid.",
-    20: "You are unable to be added to this list at this time.",
-    21: "You selected a list that does not allow duplicates. This email is in the system already, please edit that subscriber instead.",
-    22: "This e-mail address could not be unsubscribed.",
-    23: "This subscriber does not exist.",
-    24: "The link to modify your account has been sent. Please check your email.",
-    25: "The image text you typed did not register. Please go back, reload the page, and try again.",
-}
-
-
 class IActiveCampaignTool(zope.interface.Interface):
     """
     Interface to synchronise user data with with active campaign
