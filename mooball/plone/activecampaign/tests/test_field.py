839e1db73f432ea5260062884aadb2f64273fb8f
Roman Joost <roman@mooball.com>
2012-03-12 15:15:01 +1000

    New method to add custom fields

---
+++ mooball/plone/activecampaign/tests/test_field.py
@@ -0,0 +1,20 @@
+from mooball.plone.activecampaign.interfaces import IActiveCampaignField
+from mooball.plone.activecampaign.tool import ActiveCampaignField
+import unittest
+import zope.interface
+
+
+class TestFieldUnit(unittest.TestCase):
+
+    def test_interfaces(self):
+        self.assertTrue(
+            zope.interface.verify.verifyClass(
+                IActiveCampaignField, ActiveCampaignField))
+
+    def test_get_parameters(self):
+        data = dict(title=u'Field 1', type=u'Text Field', req=1,
+                    onfocus=u'', bubble_content=u'New Field')
+        field = ActiveCampaignField(**data)
+
+        data.update(perstag=u'', show_in_list=1, label=1)
+        self.assertEquals(data, field.get_parameters())
