0e708bbcfc25d8d420c27de7042788705960cfac
Alyssa Murati <alyssa@mooball.net>
2014-01-21 16:29:54 +1000

    Prepare mooball.plone.activecampaign 0.5.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.4'
+version = '0.5'
 tests_require = ['plone.app.testing',
                  'fudge',
                  'testfixtures',
